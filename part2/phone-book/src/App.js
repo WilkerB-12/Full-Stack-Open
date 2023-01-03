3c9ba246a36a93ae85f87c71d95cd5374649fcee
Wilker Blanco <wilkerblanco18@gmail.com>
2023-01-03 19:44:32 -0400

    Exercise 2.7 fnished

--- part2/phone-book/src/App.js
+++ part2/phone-book/src/App.js
@@ -1,46 +1,50 @@
-import React, { useState } from 'react'
-import Person from './components/Person'
+import React, { useState } from "react";
+import Person from "./components/Person";
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
 
-  const handleNewName = (e)=>{
-    console.log(e.target.value)
-    setNewName(e.target.value)
-  }
+  const handleNewName = (e) => {
+    console.log(e.target.value);
+    setNewName(e.target.value);
+  };
 
-  const addName=(e)=>{
+  const addName = (e) => {
     e.preventDefault();
-    const personObject={
+    const personObject = {
       name: newName,
       /*date: new Date().toISOString(),
       id: persons.length1*/
+    };
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat(personObject));
+      setNewName("");
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-  }
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleNewName}/>
+          name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-        <ul>
-          {persons.map((person,i)=> <Person key={i} person={person}></Person>)}
-        </ul>
-        <p>git addEsto es una prueba</p>
+      <ul>
+        {persons.map((person, i) => (
+          <Person key={i} person={person}></Person>
+        ))}
+      </ul>
+      <p>git add Esto es una prueba</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
